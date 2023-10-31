fe53abfbfc0adf56429a32880915d3f09c44aae0
Daniel Grießhaber <griesshaber@hdm-stuttgart.de>
2023-10-31 07:23:30 +0100

    refactor providers

--- src/providers.ts
+++ src/providers.ts
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import { Route, RouteId, RouteStateInformation } from "./types";
+import { createContext, useState } from "react";
+import { Route, RouteId, RouteInformation, RouteStateInformation } from "./types";
 
 export const EVENT_NAME = "Wintercup 2023";
 const EVENT_KEY = EVENT_NAME.toLowerCase().replaceAll(" ", "-");
-
+const ROUTES_FILE_PATH = `${process.env.PUBLIC_URL}/routes/${EVENT_KEY}.json`;
 export function useLocalStorage<T>(key: string, defaultValue: T): [T, (_: T) => void] {
     const initialValue = JSON.parse(localStorage.getItem(key) ?? "null") ?? defaultValue;
     const [state, setState] = useState(initialValue);
@@ -16,14 +16,13 @@ export function useLocalStorage<T>(key: string, defaultValue: T): [T, (_: T) => 
     return [state, setValue];
 }
 
-export function useRoutes(): [Record<RouteId, Route>, (_: Record<RouteId, Route>) => void] {
-    const event = `${EVENT_KEY}`;
+export function useRoutes(): [RouteInformation, (_: RouteInformation) => void] {
     const UPDATE_THRESHOLD = 1000 * 60;
-    const [lastUpdated, setLastUpdated] = useLocalStorage<number>(`routes-${event}-lastUpdated`, 0);
-    const [routes, setRoutes] = useLocalStorage<Record<RouteId, Route>>(`routes-${event}`, {});
+    const [lastUpdated, setLastUpdated] = useLocalStorage<number>(`routes-${EVENT_KEY}-lastUpdated`, 0);
+    const [routes, setRoutes] = useLocalStorage<RouteInformation>(`routes-${EVENT_KEY}`, {});
 
     if ((Date.now() - lastUpdated) >= UPDATE_THRESHOLD) {
-        fetch(`routes/${event}.json`)
+        fetch(ROUTES_FILE_PATH)
             .then(data => data.json())
             .then(routes => setRoutes(routes))
             .then(() => setLastUpdated(Date.now()));
@@ -33,5 +32,7 @@ export function useRoutes(): [Record<RouteId, Route>, (_: Record<RouteId, Route>
 }
 
 export function useRouteState(): [RouteStateInformation, (_: RouteStateInformation) => void] {
+
+    const RouteStateContext = createContext<RouteStateInformation>({});
     return useLocalStorage<RouteStateInformation>(`${EVENT_KEY}-state`, {});
 }
